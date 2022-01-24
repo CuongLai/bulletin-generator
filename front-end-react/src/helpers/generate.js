3889addb9a4fe21d43a10d0b2be7ce0ba30daeed
Cuong <Laic5000@gmail.com>
2022-01-23 21:57:25 -0500

    remove react front end

--- front-end-react/src/helpers/generate.js
+++
@@ -1,9 +0,0 @@
-import * as axios from 'axios';
-
-export default async function generate(text) {
-    console.log(text);
-    const res = await axios.post('http://localhost:3001/generate', {
-        frontCoverText: text,
-    });
-    console.log(res);
-}
